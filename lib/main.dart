1a82799bf370f06c7a045614fa33cb98bd2f68c8
WangYi <919937690@qq.com>
2024-01-20 00:31:15 +0800

    增加开屏遮罩；增加提示弹窗；判断登录状态从而跳转不同页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'dart:io';
+import 'package:bot_toast/bot_toast.dart';
+import 'package:excuse_demo/views/event/event.dart';
+import 'package:excuse_demo/views/initial/change_pwd.dart';
+import 'package:excuse_demo/views/initial/login.dart';
+import 'package:excuse_demo/views/initial/register.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  if (Platform.isAndroid) {
+    var style = const SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(style);
+  }
   runApp(const MyApp());
 }
 
@@ -11,59 +23,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      builder: BotToastInit(),
+      navigatorObservers: [BotToastNavigatorObserver()],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SplashScreen(),
+      routes: {
+        'login':(context)=>const LoginPage(),
+        'register':(context)=>const RegisterPage(),
+        'changePwd':(context)=>const ChangePwdPage(),
+        'event':(context)=>const EventPage()
+      },
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-//   final String title;
-//
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // backgroundColor: Colors.white,
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  SharedPreferences? sharedPreferences;
+
+  @override
+  void initState(){
+    super.initState();
+    readFromStorage();
+  }
+
+  void readFromStorage() async {
+    var prefs = await SharedPreferences.getInstance();
+    var userJson = prefs.getString('user');
+    if (userJson == null || userJson.isEmpty) {
+      Future.delayed(const Duration(milliseconds: 3000), () {
+        setState(() {
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => const LoginPage()));
+        });
+      });
+    } else {
+      Future.delayed(const Duration(milliseconds: 3000), () {
+        setState(() {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const EventPage()));
+        });
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlutterLogo(
+            size: 100.0,
+          ),
+        ],
+      )),
+    );
+  }
+}
