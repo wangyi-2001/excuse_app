27358c48322875aabd25024224011692f6389f06
WangYi <919937690@qq.com>
2024-01-20 00:33:08 +0800

    完善修改密码页面框架

---
+++ lib/views/initial/change_pwd.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ChangePwdPage extends StatefulWidget {
+  const ChangePwdPage({super.key});
+
+  @override
+  State<ChangePwdPage> createState() => _ChangePwdPageState();
+}
+
+class _ChangePwdPageState extends State<ChangePwdPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
