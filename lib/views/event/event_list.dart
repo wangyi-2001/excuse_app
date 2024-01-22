79047b1980b44d633aa0f39862e33b90b6358536
WangYi <919937690@qq.com>
2024-01-22 22:25:47 +0800

    完成首页事件列表

--- lib/views/event/event_list.dart
+++ lib/views/event/event_list.dart
@@ -1,4 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'package:excuse_demo/components/cells/event/event_cell.dart';
+import 'package:excuse_demo/models/event.dart';
+import 'package:excuse_demo/service/event_service.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
@@ -7,9 +12,83 @@ class EventPage extends StatefulWidget {
   State<EventPage> createState() => _EventPageState();
 }
 
-class _EventPageState extends State<EventPage> {
+class _EventPageState extends State<EventPage>
+with AutomaticKeepAliveClientMixin{
+  List<Event> _events = [];
+
+  Future<List<Event>> _getEvents() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var eventsStr = prefs.getString("events");
+    var eventsJson = jsonDecode(eventsStr.toString());
+    _events = EventsData.fromJson(eventsJson).events;
+    print("==========${jsonEncode(_events)}==========");
+    return _events;
+  }
+
+  @override
+  // TODO: implement wantKeepAlive
+  bool get wantKeepAlive => true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getEventsList();
+
+    _getEvents().then((List<Event> eventsData) {
+      setState(() {
+        _events = eventsData;
+      });
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    super.build(context);
+    return RefreshIndicator(
+      onRefresh: () async {
+        getEventsList();
+        setState(() {
+          _getEvents().then((List<Event> eventsData) {
+            setState(() {
+              _events = eventsData;
+            });
+          });
+        });
+      },
+      child: Scaffold(
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              ListView.builder(
+                itemCount: _events.length,
+                shrinkWrap: true,
+                padding: const EdgeInsets.only(top: 16),
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return EventCell(
+                    event: _events[index],
+                    createUser: _events[index].creator,
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "没有更多了",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
 }
