386498bdefb3dbe36e1bb44481753a3e76dada65
WangYi <919937690@qq.com>
2024-01-22 22:26:38 +0800

    增加获取事件列表接口

--- lib/service/event_service.dart
+++ lib/service/event_service.dart
@@ -1,11 +1,22 @@
+import 'dart:convert';
+
 import 'package:bot_toast/bot_toast.dart';
 import 'package:dio/dio.dart';
 import 'package:excuse_demo/http/options.dart';
 import 'package:excuse_demo/models/event.dart';
 import 'package:excuse_demo/routes/event_route.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+getEventsList() async {
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  // sharedPreferences.remove("events");
+  Response response =
+      await Dio().get(HttpOptions.BASE_URL + EventRoute.eventListPath);
+  sharedPreferences.setString("events", jsonEncode(response.data));
+}
 
 createEventService(int userId, String location, String details, int urgency,
-    int pattern, double commission) async {
+    int pattern, String commission) async {
   Map<String, dynamic> queryParameters = {
     "userid": userId,
     "location": location,
