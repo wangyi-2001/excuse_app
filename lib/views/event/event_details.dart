86807942ff631da8481f5086b4736e3218ba44ef
WangYi <919937690@qq.com>
2024-01-22 22:25:31 +0800

    搭建事件详情页面脚手架

---
+++ lib/views/event/event_details.dart
@@ -0,0 +1,16 @@
+import 'package:excuse_demo/models/event.dart';
+import 'package:flutter/cupertino.dart';
+
+class EventDetailsPage extends StatefulWidget {
+  const EventDetailsPage({super.key});
+
+  @override
+  State<EventDetailsPage> createState() => _EventDetailsPageState();
+}
+
+class _EventDetailsPageState extends State<EventDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
