6716e7ea00d713946b173784817d625061fef014
WangYi <919937690@qq.com>
2024-01-23 22:22:23 +0800

    完善我的页面

--- lib/views/mine/mine.dart
+++ lib/views/mine/mine.dart
@@ -1,4 +1,11 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
+import 'package:excuse_demo/common/page_jump_animation.dart';
+import 'package:excuse_demo/models/user.dart';
+import 'package:excuse_demo/service/user_service.dart';
+import 'package:excuse_demo/views/event/event_accpted_list.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MinePage extends StatefulWidget {
   const MinePage({super.key});
@@ -8,8 +15,249 @@ class MinePage extends StatefulWidget {
 }
 
 class _MinePageState extends State<MinePage> {
+  late User _user;
+
+  Future<User> _getUserInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var userStr = prefs.getString("user");
+    var userJson = jsonDecode(userStr.toString());
+    _user = UserData.fromJson(userJson).user;
+    print("=====${jsonEncode(_user)}=====");
+    return _user;
+  }
+
+  getUserBalance(){
+    if(_user.balance.isEmpty) {
+      return const Text("￥0.00");
+    }else{
+      return Text("￥${double.parse(_user.balance).toStringAsFixed(2)}");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getUserInfo().then((User user) {
+      setState(() {
+        _user = user;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.12,
+            padding:
+                const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 0),
+            child: Card(
+              color: const Color(0x00000000),
+              elevation: 0,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 100,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        _user.name,
+                        style: const TextStyle(fontSize: 28),
+                      ),
+                      Text(
+                        _user.phone,
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                      const Text("_user.email", style: TextStyle(fontSize: 15)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+            padding:
+                const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 15),
+            child: Card(
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: (MediaQuery.of(context).size.width - 40) / 4,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: const Text("我发布的"),
+                        ),
+                        Container(
+                          child: const Text("0"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 1,
+                    height: 30,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    width: (MediaQuery.of(context).size.width - 40) / 4,
+                    child: GestureDetector(
+                      onTap: (){
+                        Navigator.push(
+                          context,
+                          CustomRouteSlideRight(AcceptedEventsPage()),
+                        );
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            child: const Text("我接到的"),
+                          ),
+                          Container(
+                            child: const Text("0"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 1,
+                    height: 30,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    width: (MediaQuery.of(context).size.width - 40) / 4,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("免费次数"),
+                        Text("${_user.remainingTimes}"),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 1,
+                    height: 30,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    width: (MediaQuery.of(context).size.width - 40) / 4,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: const Text("钱包余额"),
+                        ),
+                        Container(
+                          child: getUserBalance(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            padding:
+                const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 15),
+            child: Card(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 60,
+                    child: Text("1"),
+                  ),
+                  Container(
+                    color: Colors.black,
+                    height: 1.0,
+                  ),
+                  const SizedBox(
+                    height: 60,
+                    child: Text("2"),
+                  ),
+                  Container(
+                    color: Colors.black,
+                    height: 1.0,
+                  ),
+                  const SizedBox(
+                    height: 60,
+                    child: Text("3"),
+                  ),
+                  Container(
+                    color: Colors.black,
+                    height: 1.0,
+                  ),
+                  const SizedBox(
+                    height: 60,
+                    child: Text("4"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width / 2,
+                height: 80,
+                padding: const EdgeInsets.only(
+                    left: 30, right: 10, top: 20, bottom: 5),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "更新信息",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width / 2,
+                height: 80,
+                padding: const EdgeInsets.only(
+                    left: 10, right: 30, top: 20, bottom: 5),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    logoutService(_user.id);
+                    prefs.remove("user");
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, "/login", (route) => false);
+                  },
+                  child: const Text(
+                    "退出登录",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
