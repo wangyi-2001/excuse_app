896e01b76eafc4d1052612e5309955d979375f55
WangYi <919937690@qq.com>
2024-01-25 22:10:12 +0800

    新增接受事件、获取已发布事件列表、删除事件接口；添加EventBus单播通道获取数据并向订阅者发送

--- lib/service/event_service.dart
+++ lib/service/event_service.dart
@@ -3,8 +3,11 @@ import 'dart:convert';
 import 'package:bot_toast/bot_toast.dart';
 import 'package:dio/dio.dart';
 import 'package:excuse_demo/http/options.dart';
+import 'package:excuse_demo/main.dart';
 import 'package:excuse_demo/models/event.dart';
 import 'package:excuse_demo/routes/event_route.dart';
+import 'package:excuse_demo/views/event/event_list.dart';
+import 'package:excuse_demo/views/mine/mine.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 getEventsList() async {
@@ -12,29 +15,73 @@ getEventsList() async {
   // sharedPreferences.remove("events");
   Response response =
       await Dio().get(HttpOptions.BASE_URL + EventRoute.eventListPath);
+  eventBus.fire(GetLatestEventsList(EventsData.fromJson(response.data).events));
   sharedPreferences.setString("events", jsonEncode(response.data));
 }
 
-createEventService(int userId, String location, String details, int urgency,
+createEvent(int userId, String location, String details, int urgency,
     int pattern, String commission) async {
   Map<String, dynamic> queryParameters = {
-    "userid": userId,
+    "userId": userId,
     "location": location,
     "details": details,
     "urgency": urgency,
     "pattern": pattern,
     "commission": commission,
   };
-  Response response = await Dio().get(
+  Response response = await Dio().post(
       HttpOptions.BASE_URL + EventRoute.eventCreatePath,
       queryParameters: queryParameters);
+  eventBus
+      .fire(GetLatestUserInfo(EventData.fromJson(response.data).event.creator));
+  getEventsList();
   BotToast.showText(text: EventData.fromJson(response.data).message);
 }
 
-getAcceptedEventsList()async{
+deleteEvent(int eventId) async {
+  Map<String, dynamic> queryParameters = {
+    "eventId": eventId,
+  };
+  Response response = await Dio().post(
+      HttpOptions.BASE_URL + EventRoute.eventDeletePath,
+      queryParameters: queryParameters);
+  getEventsList();
+  BotToast.showText(text: EventData.fromJson(response.data).message);
+}
+
+acceptEvent(int eventId, int userId) async {
+  Map<String, dynamic> queryParameters = {
+    "eventId": eventId,
+    "recipientId": userId,
+  };
+  Response response = await Dio().post(
+      HttpOptions.BASE_URL + EventRoute.eventAcceptPath,
+      queryParameters: queryParameters);
+  eventBus.fire(
+      GetLatestUserInfo(EventData.fromJson(response.data).event.recipient));
+  getEventsList();
+  BotToast.showText(text: EventData.fromJson(response.data).message);
+}
+
+getAcceptedEventsList(int userId) async {
   SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  Map<String, dynamic> queryParameters = {
+    "recipientId": userId,
+  };
   // sharedPreferences.remove("acceptedEvents");
-  Response response =
-      await Dio().get(HttpOptions.BASE_URL + EventRoute.eventAcceptedPath);
+  Response response = await Dio().get(
+      HttpOptions.BASE_URL + EventRoute.eventAcceptedPath,
+      queryParameters: queryParameters);
   sharedPreferences.setString("acceptedEvents", jsonEncode(response.data));
+}
+
+getPublishedEventsList(int userId) async {
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  Map<String, dynamic> queryParameters = {
+    "creatorId": userId,
+  };
+  Response response = await Dio().get(
+      HttpOptions.BASE_URL + EventRoute.eventPublishedPath,
+      queryParameters: queryParameters);
+  sharedPreferences.setString("publishedEvents", jsonEncode(response.data));
 }
