91cf941301f4cdab93a93e446a1ea17ebeafc008
WangYi <919937690@qq.com>
2024-01-25 22:05:41 +0800

    新增编辑已有事件的框架

---
+++ lib/views/event/event_edit.dart
@@ -0,0 +1,18 @@
+
+import 'package:excuse_demo/models/event.dart';
+import 'package:flutter/cupertino.dart';
+
+class EventEditPage extends StatefulWidget {
+  final Event event;
+  const EventEditPage({super.key, required this.event});
+
+  @override
+  State<EventEditPage> createState() => _EventEditPageState();
+}
+
+class _EventEditPageState extends State<EventEditPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
